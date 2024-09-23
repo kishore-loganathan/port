312b8a5c50bb594d828ceee66d2a6ae7d900a1ac
kishore-loganathan <kishoreloganathan773@gmail.com>
2024-09-23 05:09:42 -0700

    ksakmal

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,5 +3,9 @@ import React from "react";
 const Contact = () => {
     return (
         <div className="">Contact</div>
+        <div className="">Contact</div>
+        <div className="">Contact</div>
+        <div className="">Contact</div>
+        <div className="">Contact</div>
     )
 }
